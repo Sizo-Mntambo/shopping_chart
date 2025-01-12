45f9dbcb2cffd5e4d0154c66aae3ce9aa4a9363d
Sizo-Mntambo <>
2025-01-12 18:39:48 +0200

    partial functions
    creates cart

--- cart.py
+++ cart.py
@@ -1,15 +1,16 @@
 menu = {'apple': 5, 'orange': 6, 'kiwi': 10, 'banana': 3, 'peach': 8, 'dragon fruit': 18}
 cart = []
 total = 0
+setted = [(fruit, price) for fruit, price in menu.items()]
 
-while True:
-    setted = [(fruit, price) for fruit, price in menu.items()]
+while True:    
     order = input("Enter name of fruit(q to quit): ")
-    for sub in setted:
-        if order in setted:
-            cart.append(order)
+    for item, cash in setted:        
+        if order == item:
+            cart.append((item, cash))
             
     if order.lower() == 'q':
         break
+
         
         
